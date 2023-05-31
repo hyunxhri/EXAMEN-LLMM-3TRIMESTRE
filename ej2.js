a94ce61c0e4a488671a2778548be56c3401b68eb
hyunxhri <sbulmar842@g.educaand.es>
2023-05-31 19:17:44 +0200

    ejercicio2 funcional

--- ej2.js
+++ ej2.js
@@ -1,32 +1,59 @@
-const validaNombre = () => {
-    let nombre = document.forms["formulario"]["nombre"].value
-    alert(nombre)
+const nombresArray = []
+const dnisArray = []
+const correosArray = []
+
+const validaNombre = (nombre) => {
     let nombreRegex = /^.{3,}$/
     if (nombre.match(nombreRegex)) return true
     else return false
 }
 
-const validaDNI = () => {
-    let dni = document.forms["formulario"]["dni"].value
-    alert(dni)
+const validaDNI = (dni) => {
     let dniRegex = /^\d{8}[a-zA-Z]$/
     if (dni.match(dniRegex)) return true
     else return false
 }
 
-const validaCorreo = () => {
-    let correo = document.forms["formulario"]["email"].value
-    alert(correo)
+const validaCorreo = (correo) => {
     let correoRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
     if (correo.match(correoRegex)) return true
     else return false
 }
 
 const form = () => {
-    let validacionNombre = validaNombre()
-    let validacionDNI = validaDNI()
-    let validacionCorreo = validaCorreo()
-    alert(validacionNombre)
-    alert(validacionDNI)
-    alert(validacionCorreo)
+    event.preventDefault()
+
+    let nombre = document.forms["formulario"]["nombre"].value
+    let dni = document.forms["formulario"]["dni"].value
+    let correo = document.forms["formulario"]["email"].value
+
+    if(!validaNombre(nombre)){
+        alert("El nombre no es correcto.")
+        return
+    } 
+
+    if(!validaDNI(dni)){
+        alert("El DNI no es correcto.")
+        return
+    }
+
+    if(!validaCorreo(correo)){
+        alert("El correo no es correcto.")
+        return
+    }
+
+    nombresArray.push(nombre)
+    dnisArray.push(dni)
+    correosArray.push(correo)
+
+    filaTabla = document.getElementById("cuerpoTabla").insertRow(-1)
+    filaTabla.insertCell(0).innerHTML = nombre
+    filaTabla.insertCell(1).innerHTML = dni
+    filaTabla.insertCell(2).innerHTML = correo
+
+}
+
+const listarArray = () => {
+    let nuevaVentana = window.open()
+    nuevaVentana.document.write(`Nombres: ${nombresArray} <br>DNIs: ${dnisArray} <br>Correos: ${correosArray}`)
 }
